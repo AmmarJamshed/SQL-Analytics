d008985e1dc7e46ee44db3b2eb51a855df42432e
Muhammad Ammar Jamshed <ammarjamshed123@gmail.com>
2022-03-18 04:46:36 +0500

    Basic Data Manipulation

---
+++ sqlscript.sql
@@ -0,0 +1,169 @@
+REM   Script: Data Manipulation with SQL
+REM   Basic data manipulation
+
+select EMPLOYEES;
+
+select * EMPLOYEES;
+
+select from EMPLOYEES;
+
+select name  
+from employees;
+
+Create TABLE AJ 
+( 
+Name VARCHAR, 
+ID int 
+);
+
+CREATE TABLE Persons ( 
+    PersonID int, 
+    LastName varchar(255), 
+    FirstName varchar(255), 
+    Address varchar(255), 
+    City varchar(255) 
+);
+
+CREATE TABLE Persons ( 
+    PersonID int, 
+    LastName varchar(255), 
+    FirstName varchar(255), 
+    Address varchar(255), 
+    City varchar(255) 
+);
+
+INSERT INTO Persons( 
+VALUES (1,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi') 
+VALUES (1,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi') 
+VALUES (1,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi') 
+VALUES (1,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi') 
+);
+
+CREATE TABLE Persons ( 
+    PersonID int, 
+    LastName varchar(255), 
+    FirstName varchar(255), 
+    Address varchar(255), 
+    City varchar(255) 
+);
+
+INSERT INTO Persons( 
+VALUES (1,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi') 
+VALUES (2,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi') 
+VALUES (3,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Lahore') 
+VALUES (4,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi') 
+VALUES (5,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Multan') 
+VALUES (6,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi') 
+);
+
+CREATE TABLE Persons ( 
+    PersonID int, 
+    LastName varchar(255), 
+    FirstName varchar(255), 
+    Address varchar(255), 
+    City varchar(255) 
+);
+
+INSERT INTO Persons 
+VALUES (1,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi') 
+VALUES (2,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi') 
+VALUES (3,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Lahore') 
+VALUES (4,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi') 
+VALUES (5,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Multan') 
+VALUES (6,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi');
+
+CREATE TABLE Persons ( 
+    PersonID int, 
+    LastName varchar(255), 
+    FirstName varchar(255), 
+    Address varchar(255), 
+    City varchar(255) 
+);
+
+INSERT INTO Persons 
+VALUES (1,'xnjkqnejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi') 
+VALUES (2,'xnjkgnejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi') 
+VALUES (3,'xnjknsejw','dxmeidfkcjnerkj','cmekjcnkej','Lahore') 
+VALUES (4,'xnjknejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi') 
+VALUES (5,'xndjknejw','dxmeidfkcjnerkj','cmekjcnkej','Multan') 
+VALUES (6,'xnhjknejw','dxmeidfkcjnerkj','cmekjcnkej','Karachi');
+
+INSERT INTO Persons (PersonID,LastName,FirstName,Address,City) 
+VALUES (1,'xnjkqnejw','dxmeidfkcjnerkj','cmekrrjcnkej','Karachi') 
+VALUES (2,'xnjkgnejw','dxmeidfkcjnerkj','cmekjcwwnkej','Karachi') 
+VALUES (3,'xnjknsejw','dxmeidfkcjnerkj','cmekjzzcnkej','Lahore') 
+VALUES (4,'xnjknejw','dxmeidfkcjnerkj','cmekjcllnkej','Karachi') 
+VALUES (5,'xndjknejw','dxmeidfkcjnerkj','cmekoojcnkej','Multan') 
+VALUES (6,'xnhjknejw','dxmeidfkcjnerkj','cmekjbbcnkej','Karachi');
+
+CREATE TABLE Persons ( 
+    PersonID int, 
+    LastName varchar(255), 
+    FirstName varchar(255), 
+    Address varchar(255), 
+    City varchar(255) 
+);
+
+INSERT INTO Persons (PersonID,LastName,FirstName,Address,City) 
+VALUES (6,'xnhjknejw','dxmeidfkcjnerkj','cmekjbbcnkej','Karachi');
+
+INSERT INTO Persons (PersonID,LastName,FirstName,Address,City) 
+VALUES (6,'xnhjknejw','dxmeidfkcjnerkj','cmekjbbcnkej','Karachi');
+
+INSERT INTO Persons (PersonID,LastName,FirstName,Address,City) 
+VALUES (1,'xnjkqnejw','dxmeidfkcjnerkj','cmekrrjcnkej','Karachi') 
+VALUES (2,'xnjkgnejw','dxmeidfkcjnerkj','cmekjcwwnkej','Karachi') 
+VALUES (3,'xnjknsejw','dxmeidfkcjnerkj','cmekjzzcnkej','Lahore') 
+VALUES (4,'xnjknejw','dxmeidfkcjnerkj','cmekjcllnkej','Karachi') 
+VALUES (5,'xndjknejw','dxmeidfkcjnerkj','cmekoojcnkej','Multan');
+
+INSERT INTO Persons (PersonID,LastName,FirstName,Address,City) 
+VALUES (1,'xnjkqnejw','dxmeidfkcjnerkj','cmekrrjcnkej','Karachi');
+
+INSERT INTO Persons (PersonID,LastName,FirstName,Address,City) 
+VALUES (2,'xnjkgnejw','dxmeidfkcjnerkj','cmekjcwwnkej','Karachi');
+
+INSERT INTO Persons (PersonID,LastName,FirstName,Address,City) 
+VALUES (3,'xnjknsejw','dxmeidfkcjnerkj','cmekjzzcnkej','Lahore');
+
+INSERT INTO Persons (PersonID,LastName,FirstName,Address,City) 
+VALUES (4,'xnjknejw','dxmeidfkcjnerkj','cmekjcllnkej','Karachi');
+
+INSERT INTO Persons (PersonID,LastName,FirstName,Address,City) 
+VALUES (5,'xndjknejw','dxmeidfkcjnerkj','cmekoojcnkej','Multan');
+
+VIEW Persons;
+
+
+SELECT * Persons;
+
+SELECT * * FROM Persons;
+
+SELECT *  FROM Persons;
+
+SELECT FIRSTNAME FROM PERSONS 
+WHERE FIRSTNAME = '%j';
+
+SELECT *  FROM Persons;
+
+SELECT FIRSTNAME FROM PERSONS 
+WHERE FIRSTNAME = 'j%';
+
+SELECT FROM PERSONS 
+WHERE FIRSTNAME = 'j%';
+
+SELECT * FROM PERSONS 
+WHERE FIRSTNAME = 'j%';
+
+SELECT * FROM PERSONS 
+WHERE City = '%i';
+
+SELECT * FROM PERSONS 
+WHERE City = 'Karachi';
+
+SELECT * FROM PERSONS 
+WHERE City LIKE '%i';
+
+SELECT * FROM PERSONS 
+WHERE City LIKE '%n';
+
