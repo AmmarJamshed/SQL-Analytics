a291f0946ed1431fe7406f1563581c2134044f04
Muhammad Ammar Jamshed <ammarjamshed123@gmail.com>
2022-03-30 14:06:41 +0500

    Data Manipulation

---
+++ Near to intermediate queries.sql
@@ -0,0 +1,10 @@
+select * from newschema.student;
+
+select * from newschema.student
+WHERE Fjob = 'other' AND reason = 'course';
+
+select sex, G1, age
+FROM newschema.student
+WHERE G1 > 10
+ORDER BY age;
+
